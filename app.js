4d9cfefbe1fcd8a364e03231e20b4553612e789b
aldinursyahdin <72461590+aldinursyahdin@users.noreply.github.com>
2023-08-18 22:57:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+ origin:["https://gigih-final-project-eta.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+}));
 
 
 
